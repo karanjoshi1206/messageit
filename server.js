00354412b5d5aadb1de69b035dab1978ad97a637
karan1206 <64606159+karanjoshi1206@users.noreply.github.com>
2021-01-10 18:04:17 +0530

    bug

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const http = require("http")
 const path = require("path")
-const port = 3000 || process.env.PORT;
+const port = process.env.PORT || 3000;
 const socketio = require("socket.io")
 const formatMessage = require("./utils/messages")
 const { userJoin, getCurrentUser, userLeave, getRoomUsers } = require("./utils/users")
